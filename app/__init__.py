777fbbb208b0296b741895ddf029be0e7bdb1716
carol-profile <carolgitonga93@gmail.com>
2022-05-04 23:10:11 +0300

    create app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,16 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-
-    
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
     return app
